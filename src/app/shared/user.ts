f5956f5d31ea39390b75eed447e8e74aa7c8af83
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-08-07 17:16:56 -0300

    2 files modified

--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -3,7 +3,13 @@ import {Address} from "./address";
 export class User implements Address{
   name: string;
   email: string;
-  address: LoginData;
+  tel: string;
+  city: string;
+  street: string;
+  num: string;
+  complement: string;
+  country: string;
+  state: string;
   logged: boolean;
   username: string;
   password: string;
