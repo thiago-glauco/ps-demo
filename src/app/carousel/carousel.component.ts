057065ffbafdd816fac437b873c6b86bb0feed43
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-08-19 12:08:28 -0300

    2 files modified

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
-
+  @ViewChild('topCarousel') topCarousel: CarouselComponent;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  reestartCarousel($event) {
+    this.topCarousel.slideTo(0);
   }
 
 }
