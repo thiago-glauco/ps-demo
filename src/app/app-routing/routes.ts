129a3bae53465b69cde7dea778b73bc5c54db137
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-10-08 08:45:27 -0300

    view home

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from '../views/home/home.component';
+import { AppRoutingModule } from '../app-routing/app-routing.module';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full'}
+];
