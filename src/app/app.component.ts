a9569013bd7b3f6035ccf4ecb0d12c2a7848324a
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-11-09 13:46:03 -0300

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  transition,
+  trigger,
+  query,
+  style,
+  animate,
+  group,
+  animateChild
+} from '@angular/animations';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  animations: [
+    trigger('myAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [style({ opacity: 0 })],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+          [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+          { optional: true }
+        )
+      ])
+    ])
+  ] // register the animations
+
 })
 export class AppComponent  {
   name = 'Angular';
